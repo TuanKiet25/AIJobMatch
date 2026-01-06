4903a977612fc4b78824d2dd3bea6f3fbdd84f4f
TuanKiet25 <kietntse183810@fpt.edu.vn>
2026-01-06 23:16:40 +0700

    UpdateAccountEntity

--- AIJobMatch.Domain/Entities/Account.cs
+++ AIJobMatch.Domain/Entities/Account.cs
@@ -9,9 +9,11 @@ namespace AIJobMatch.Domain.Entities
 {
     public class Account : BaseEntity
     {
-        public string? Username { get; set; }
+        public string? FullName { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
         public string? PasswordHash { get; set; }
         public Role Role { get; set; }
     }
