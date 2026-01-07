aa683263814e427519833a9bed1e88b4d9e29d53
TuanKiet25 <kietntse183810@fpt.edu.vn>
2026-01-07 19:47:43 +0700

    AddressFluentAPI

---
+++ AIJobMatch.Infrastructure/Configurations/AccountConfig.cs
@@ -0,0 +1,23 @@
+﻿using AIJobMatch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIJobMatch.Infrastructure.Configurations
+{
+    public class AccountConfig : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.HasMany(a => a.Addresses)
+                   .WithOne(add => add.Account)
+                   .HasForeignKey(add => add.AccountId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ AIJobMatch.Infrastructure/Configurations/WardConfig.cs
@@ -0,0 +1,26 @@
+﻿using AIJobMatch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIJobMatch.Infrastructure.Configurations
+{
+    public class WardConfig : IEntityTypeConfiguration<Ward>
+    {
+        public void Configure(EntityTypeBuilder<Ward> builder)
+        {
+            builder.HasKey(w => w.WardCode);
+            builder.HasMany(w => w.Addresses)
+                   .WithOne(a => a.Ward)
+                   .HasForeignKey(a => a.WardCode)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+
+
+    }
+}
+
