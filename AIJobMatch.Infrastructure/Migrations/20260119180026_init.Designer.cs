8648f7a477ab8f65c6dc11ebf25218f763efd2c1
TuanKiet25 <kietntse183810@fpt.edu.vn>
2026-01-20 10:45:55 +0700

    .

--- AIJobMatch.Infrastructure/Migrations/20260119180026_init.Designer.cs
+++ AIJobMatch.Infrastructure/Migrations/20260119180026_init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AIJobMatch.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260117183248_init")]
+    [Migration("20260119180026_init")]
     partial class init
     {
         /// <inheritdoc />
@@ -231,7 +231,7 @@ namespace AIJobMatch.Infrastructure.Migrations
                     b.Property<Guid>("AccountId")
                         .HasColumnType("uniqueidentifier");
 
-                    b.Property<Guid>("CompanyId")
+                    b.Property<Guid?>("CompanyId")
                         .HasColumnType("uniqueidentifier");
 
                     b.HasKey("AccountId");
@@ -329,8 +329,7 @@ namespace AIJobMatch.Infrastructure.Migrations
                     b.HasOne("AIJobMatch.Domain.Entities.Company", "Company")
                         .WithMany("Recruiters")
                         .HasForeignKey("CompanyId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+                        .OnDelete(DeleteBehavior.Cascade);
 
                     b.Navigation("Account");
 
