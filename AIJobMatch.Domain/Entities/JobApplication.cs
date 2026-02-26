f9bd1d61821389aea6ae5abea22cdd9ec47f8794
TuanKiet25 <kietntse183810@fpt.edu.vn>
2026-02-26 23:13:08 +0700

    AICvSuggestion

---
+++ AIJobMatch.Domain/Entities/JobApplication.cs
@@ -0,0 +1,26 @@
+﻿using AIJobMatch.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIJobMatch.Domain.Entities
+{
+    public class JobApplication : BaseEntity
+    {
+        public Guid JobPostingId { get; set; }
+        public Guid CandidateProfileId { get; set; }
+        public Guid CandidateId { get; set; }
+        public JobPosting? JobPosting { get; set; }
+        public CandidateProfile? CandidateProfile { get; set; }
+        public Candidate? Candidate { get; set; }
+        public JobApplicationStatus Status { get; set; } = JobApplicationStatus.Pending;
+        public string ProfilesSnapshot { get; set; } 
+        public string? CVUrl { get; set; }
+        public double MatchScore { get; set; } = 0;
+        public string? AIAnalysis { get; set; }
+        public string? CoverLetter { get; set; }    
+
+    }
+}
